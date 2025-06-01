55803f3327e1c1a1db2ac79e9998d843bd9ff64b
Mohit Yadav <mohityadav7240726458@gmail.com>
2025-06-02 01:26:37 +0530

    API BASE URL change for hosting

--- FrountEnd/src/services/api.ts
+++ FrountEnd/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosResponse, type AxiosError } from "axios"
 import type { AuthResponse, ApiResponse, Note, CreateNoteData, UpdateNoteData, NotesStats } from "../types"
 
-const API_BASE_URL = "http://localhost:5000/api"
+const API_BASE_URL = "https://keepnotesbackend-i6de.onrender.com/api"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
