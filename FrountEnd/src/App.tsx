eba6d31cbd86db66a7880707e4a962a79917e297
Mohit Yadav <mohityadav7240726458@gmail.com>
2025-06-01 14:03:16 +0530

    create structure for frountend

--- FrountEnd/src/App.tsx
+++ FrountEnd/src/App.tsx
@@ -1,18 +1,26 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-// import { Button } from "./components/ui/button"
-// import { Card } from './components/ui/card'
+// client/src/App.tsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import NotePage from './pages/NotePage';
+import NewNotePage from './pages/NewNotePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import Layout from './components/Layout';
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="notes/new" element={<NewNotePage />} />
+          <Route path="notes/:id" element={<NotePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
